7c9694b285d2604c308998e4f0e1f68661cb527c
Константин Кучинский <konstantin.kuchinskii@vokigames.com>
2021-03-26 03:32:17 +0200

    ++
    ++

---
+++ Assets/Scripts/CircleInstantiate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleInstantiate : MonoBehaviour
+{
+    public GameObject Template;
+    public int Count;
+    private GameObject[] _spownPoints;
+
+    private void Start()
+    {
+        var SpawnEnemyJob = StartCoroutine(SpawnEnemy());
+
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        _spownPoints = GameObject.FindGameObjectsWithTag("EnemySpawn");
+
+        for (int i = 0; i < Count; i++)
+        {
+            var spawnPoint = Random.Range(0, _spownPoints.Length);
+            GameObject gameObject = Instantiate(Template, _spownPoints[spawnPoint].transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    [HideInInspector] public SpriteRenderer Target;
+    [SerializeField] private float _duration;
+
+    [SerializeField] private Color _targetColor;
+    private float _ranningTime;
+    private Color _startColor;
+
+    private void Start()
+    {
+        Target = GetComponent<SpriteRenderer>();
+        _startColor = Target.color;
+    }
+
+
+    private void Update()
+    {
+        if (_ranningTime <= _duration)
+        {
+            _ranningTime += Time.deltaTime;
+
+            float normalizeRunningTime = _ranningTime / _duration;
+
+            
+            Target.color = Color.Lerp(_startColor, _targetColor, normalizeRunningTime);
+        }
+    }
+}
